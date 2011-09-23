9c92aa62d94406a54a68db53cb8463d8858588f0
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2011-09-23 01:12:16 -0700

    Made the Scala bindings build properly.

---
+++ examples/src/main/scala/com/asgow/ciel/examples/scala/GeneratorTest.scala
@@ -0,0 +1,24 @@
+package com.asgow.ciel.examples.scala {
+
+import com.asgow.ciel.executor.Ciel
+import com.asgow.ciel.tasks._
+import com.asgow.ciel.scala._
+import com.asgow.ciel.references._
+
+class ScalaGeneratorTest extends SkylaThread[Int] {
+  override def run = {
+
+    val a = Skyla.spawnGenerator[Int] { _yield =>
+      _yield(6)
+      _yield(3)
+      _yield(7)
+    }
+
+    val iter = a.iterator 
+    val total = iter.sum
+
+    total
+  }
+}
+
+}
